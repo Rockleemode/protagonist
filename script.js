2a28b8cb3c9f421eb0321cb67f61362046827d95
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-07-18 23:43:52 +0100

    arrange the images in a slide format

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const upBtn = document.querySelector(".up-btn");
+const downBtn = document.querySelector(".down-btn");
