64054b83a0c09e74574caa0a11431bd9da0129f4
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-08-06 23:12:04 +0100

    adjusted the images dimensions

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const upBtn = document.querySelector(".up-btn");
 const downBtn = document.querySelector(".down-btn");
+const slideLength = right.querySelectorAll("div").length
+
+let active = 0;
+
+left.style.top = `-${(slideLength-1)*100}vh`
+
+upBtn.addEventListener("click",() => change("up"))
+downBtn.addEventListener("click",() => change('down'))   
+
+const change = (dir) =>{
+    const height = container.clientHeight
+    if (dir == "up"){
+            active++
+            if(active > slideLength -  1){
+                active = 0 
+            }
+         } else if (dir == 'down') {
+              active--
+            }
+            if(active < 0){
+                active = slideLength - 1;
+    }
+
+    right.style.transform = `translateY(-${active * height}px)`
+    left.style.transform = `translateY(${active * height}px)`
+            
+}
+
